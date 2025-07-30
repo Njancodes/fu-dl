15330e684bc47a63f0519259a88bc3ad05adfc5f
njancodes <njansmailbox@gmail.com>
2025-07-30 13:07:21 +0530

    Update the ejs based on the files uploaded

--- index.js
+++ index.js
@@ -8,24 +8,27 @@ import multer from 'multer'
 const app = express()
 const upload = multer()
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(import.meta.dirname, 'public')));
 app.set('view engine', 'ejs')
-app.set('views','./views')
+app.set('views', './views')
 
-app.get("/",(req,res)=>{
-    const data = {
-        files:["Hello.jpg","Hi.jpg"]
-    }
+const data = {
+    files: [{name:"H.jpg",id:1},{name:"He.jpg",id:2},{name:"Hel.jpg",id:3}]
+}
 
-    res.render('index',data)
+app.get("/", (req, res) => {
+    res.render('index', data)
 })
 
-app.post("/",upload.single("file"),(req,res)=>{
+app.post("/", upload.single("file"), (req, res) => {
     console.log(req.file)
-    res.status(204).send();
+    data.files.push({name:req.file.originalname, id:data.files.length + 1});
+    res.render('index', data)
 })
 
-app.listen(3000, ()=>{
+
+
+app.listen(3000, () => {
     console.log("The server is live at http://localhost:3000")
 })
